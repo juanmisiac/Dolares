16861ddc850ddda89675b273cb855685830e58fc
juanmisiac <145512633+juanmisiac@users.noreply.github.com>
2024-02-17 15:32:09 -0300

    Funcion fetch mejorada
    Se añadió la capacidad de actualizar las cotizaciones cada un minuto sin que el  usuario deba hacer nada

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -1,10 +1,19 @@
-fetch("https://dolarapi.com/v1/dolares")
-    .then(response => response.json())
-    .then(data => {
-        const monedas = ["oficial", "blue", "bolsa", "ccl", "mayorista", "cripto", "tarjeta"];
+function getCotizaciones() {
+    fetch("https://dolarapi.com/v1/dolares")
+        .then(response => response.json())
+        .then(data => {
+            const monedas = ["oficial", "blue", "bolsa", "ccl", "mayorista", "cripto", "tarjeta"];
 
-        monedas.forEach(m => {
-            const elemento = document.querySelector(`#${m}`);
-            elemento.innerHTML = `<td>Compra: $${data[monedas.indexOf(m)].compra} <br> Venta: $${data[monedas.indexOf(m)].venta}</td>`;
+            monedas.forEach(m => {
+                const elemento = document.querySelector(`#${m}`);
+                elemento.innerHTML = `<td>Compra: $${data[monedas.indexOf(m)].compra} <br> Venta: $${data[monedas.indexOf(m)].venta}</td>`;
+            });
+        })
+        .catch(error => {
+            console.error('Se produjo un error al obtener los datos de cotización:', error);
         });
-    });
+}
+
+getCotizaciones();
+
+setInterval(getCotizaciones, 1 * 60 * 1000); // Actualiza los datos cada 1 minuto (expresado en milisegundos)
