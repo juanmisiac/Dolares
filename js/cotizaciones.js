01d671cf2e67fa398c200a62ce397f6d369c3d50
juanmisiac <145512633+juanmisiac@users.noreply.github.com>
2024-03-02 14:20:08 -0300

    Preparativos
    Se añadieron preparativos para la actualización de conversión

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -1,8 +1,9 @@
+const monedas = ["oficial", "blue", "bolsa", "ccl", "mayorista", "cripto", "tarjeta"];
+
 function getCotizaciones() {
     fetch("https://dolarapi.com/v1/dolares")
         .then(response => response.json())
         .then(data => {
-            const monedas = ["oficial", "blue", "bolsa", "ccl", "mayorista", "cripto", "tarjeta"];
 
             monedas.forEach(m => {
                 const elementoDesktop = document.querySelector(`#${m}`);
@@ -12,6 +13,7 @@ function getCotizaciones() {
                 
                 elementoMobile.textContent = `Compra: $${data[monedas.indexOf(m)].compra} ----- Venta: $${data[monedas.indexOf(m)].venta}`;
             });
+        
         })
         .catch(error => {
             console.error('Se produjo un error al obtener los datos de cotización:', error);
@@ -22,4 +24,17 @@ getCotizaciones();
 
 setInterval(getCotizaciones, 1 * 60 * 1000); // Actualiza los datos cada 1 minuto (expresado en milisegundos)
 
+const btnOficial = document.querySelector('#btn-oficial'),
+      btnBlue = document.querySelector('#btn-blue'),
+      btnBolsa = document.querySelector('#btn-bolsa'),
+      btnCcl = document.querySelector('#btn-ccl'),
+      btnMayorista = document.querySelector('#btn-mayorista'),
+      btnCripto = document.querySelector('#btn-cripto'),
+      btnTarjeta = document.querySelector('#btn-tarjeta');
+
+/*
+btnOficial.addEventListener('click', () => {
+    console.log('$${data[monedas.indexOf("oficial")].compra}');
+});
+*/
 
