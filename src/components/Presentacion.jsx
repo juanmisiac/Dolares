33eae9ace96ebc274cf0b5ee13fe257a3407fd39
juanmisiac <145512633+juanmisiac@users.noreply.github.com>
2024-04-15 23:54:30 -0300

    new bg + debugging

--- src/components/Presentacion.jsx
+++ src/components/Presentacion.jsx
@@ -1,7 +1,7 @@
 export const Presentacion = () => {
   return (
     <div className="presentacion-section">
-      <p><b>Dólarcito</b> es el sitio donde podrás ver el valor de las cotizaciones del dólar en tiempo real. Desarrollado por Juan Misiac utilizando React y consumiendo la DolarApi desarrollada por Enzo Notario.</p>
+      <p><b>Dólarcito</b> es el sitio donde podrás ver el valor de las cotizaciones del dólar en tiempo real. Desarrollado por Juan Misiac utilizando React y consumiendo la <a href="https://dolarapi.com/docs/" target="_blank">DolarApi</a> desarrollada por Enzo Notario.</p>
       <p>Selecciona una cotización de la lista e ingresa la cantidad a convertir en nuestro conversor:</p>
     </div>
   )
