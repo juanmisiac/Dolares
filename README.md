a7775241c2771d9d09979e918d8f851489dfeec8
juanmisiac <145512633+juanmisiac@users.noreply.github.com>
2024-05-19 21:45:14 -0300

    Revert "Merge pull request #1 from juanmisiac/dolarcito-v2"
    This reverts commit 5c9deb3d8464b2949466b2f277975ccea2b01b2d, reversing
    changes made to 83a59456536911dc58293b6e8cc85b27176f172f.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 
+# Dólarcito💱
+
+Pagina que muestra las diferentes cotizaciones del Dólar en Argentina, consumiendo una API con JavaScript.
+
+<img src="https://media.licdn.com/dms/image/D4D22AQEjG5izbKKfsA/feedshare-shrink_2048_1536/0/1710634652041?e=1715817600&v=beta&t=qpQlVTUUuKH_hEckPJAFprGsi-rh_qDZqS-OwVPzkM4" width="720"/>
+
+Actualmente me encuentro reconstruyendo este proyecto desde cero utilizando React, pueden encontrar dichos avances en la rama "dolarcito-v2". A continuación, una captura de pantalla de la proxima versión:
+
+<img src="https://media.licdn.com/dms/image/D4D22AQGSPCY4S7CuEw/feedshare-shrink_2048_1536/0/1713902844814?e=1717632000&v=beta&t=Uc0He_Hf-eYZNCaC3Oaf-c384fV85-dmpxbSdmdXoJk" width="720"/>
+
+
---
+++ js/cotizaciones.js
@@ -0,0 +1,224 @@
+const monedas = ["oficial", "blue", "bolsa", "ccl", "mayorista", "cripto", "tarjeta"];
+let cotizacionesData;
+
+function getCotizaciones() {
+    fetch("https://dolarapi.com/v1/dolares")
+        .then(response => response.json())
+        .then(data => {
+
+            cotizacionesData = data;
+
+            monedas.forEach(m => {
+                const elementoDesktop = document.querySelector(`#${m}`);
+                const elementoMobile = document.querySelector(`#${m}-mobile`);
+                
+                elementoDesktop.innerHTML = `Compra: $${data[monedas.indexOf(m)].compra} <br> Venta: $${data[monedas.indexOf(m)].venta}`;
+                
+                elementoMobile.textContent = `Compra: $${data[monedas.indexOf(m)].compra} ----- Venta: $${data[monedas.indexOf(m)].venta}`;
+            });
+        
+        })
+        .catch(error => {
+            console.error('Se produjo un error al obtener los datos de cotización:', error);
+        });
+}
+
+getCotizaciones();
+
+setInterval(getCotizaciones, 1 * 60 * 1000); // Actualiza los datos cada 1 minuto (expresado en milisegundos)
+
+//Botones
+const botones = document.querySelectorAll('.boton-conversor'),
+      btnOficial = document.querySelectorAll('.btn-oficial'),
+      btnBlue = document.querySelectorAll('.btn-blue'),
+      btnBolsa = document.querySelectorAll('.btn-bolsa'),
+      btnCcl = document.querySelectorAll('.btn-ccl'),
+      btnMayorista = document.querySelectorAll('.btn-mayorista'),
+      btnCripto = document.querySelectorAll('.btn-cripto'),
+      btnTarjeta = document.querySelectorAll('.btn-tarjeta'),
+      btnExchange = document.querySelectorAll('.exchange'),
+      btnCambiarConv = document.querySelectorAll('.arrow-conv');
+
+//Conversores
+const conversorArsUsd = document.querySelector('.conversor-ars-usd'),
+      conversorUsdArs = document.querySelector('.conversor-usd-ars');
+
+//Event Listener del btnCambiarConv, para cambiar el conversor que se muestra al usuario
+btnCambiarConv.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+        if (conversorArsUsd.classList.contains('conversor-oculto')) {
+            
+            conversorArsUsd.classList.remove('conversor-oculto');
+            conversorUsdArs.classList.add('conversor-oculto');
+
+        } else if (conversorUsdArs.classList.contains('conversor-oculto')) {
+            
+            conversorUsdArs.classList.remove('conversor-oculto');
+            conversorArsUsd.classList.add('conversor-oculto');
+
+        }
+
+        console.log("Conversor cambiado");
+    });
+});
+
+//Event Listeners de los botones selectores de cotizaciónes
+btnOficial.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("oficial")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+btnBlue.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("blue")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+btnBolsa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("bolsa")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+btnCcl.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("ccl")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+btnMayorista.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("mayorista")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+btnCripto.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("cripto")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+btnTarjeta.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(`$${cotizacionesData[monedas.indexOf("tarjeta")].venta}`);
+
+        if (btn.classList.contains('boton-seleccionado')) {
+            btn.classList.remove('boton-seleccionado');
+        } else {
+            botones.forEach(boton => boton.classList.remove('boton-seleccionado'));
+            btn.classList.add('boton-seleccionado');
+        }
+
+    });
+});
+
+//Elementos de los conversores
+let inpPesoAU = document.querySelector('#cant-peso-a-u'),
+    inpDolarAU = document.querySelector('#cant-dolar-a-u'),
+    inpPesoUA = document.querySelector('#cant-peso-u-a'),
+    inpDolarUA = document.querySelector('#cant-dolar-u-a'),
+    divInputsConv = document.querySelector('.inputs-conv'),
+    valorDolarSeleccionado,
+    valorDolarPesoConvertido;
+
+const errorParagraphs = document.querySelectorAll('.error-message');            
+
+//Event Listener del btnExchange, para realizar la conversión
+btnExchange.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.add('exc-animation');
+        setTimeout(function() {
+            btn.classList.remove('exc-animation');
+        }, 1000);
+        
+        let monedaSeleccionada;
+        const btnSeleccionado = document.querySelector('.boton-seleccionado');
+
+        if (btnSeleccionado != null) {
+            
+            monedaSeleccionada = btnSeleccionado.classList[0].replace('btn-', '');
+
+            if (conversorArsUsd.classList.contains('conversor-oculto') == false) {
+                
+                valorDolarSeleccionado = cotizacionesData[monedas.indexOf(monedaSeleccionada)].venta;
+                valorDolarPesoConvertido = parseFloat((inpPesoAU.value / valorDolarSeleccionado).toFixed(2));
+                inpDolarAU.value = valorDolarPesoConvertido;
+
+            } else if (conversorUsdArs.classList.contains('conversor-oculto') == false) {
+                
+                valorDolarSeleccionado = cotizacionesData[monedas.indexOf(monedaSeleccionada)].venta;
+                valorDolarPesoConvertido = parseFloat((inpDolarUA.value * valorDolarSeleccionado).toFixed(2));
+                inpPesoUA.value = valorDolarPesoConvertido;
+
+            }
+
+            errorParagraphs.forEach(paragraph => paragraph.style.display = 'none');
+        
+        } else {errorParagraphs.forEach(paragraph => paragraph.style.display = 'block')}
+    });
+});
+
+/*
+
+COMENTARIOS DE FUTURAS ACTUALIZACIONES:
+
+-Añadir nuevo error: "Ingrese un valor a convertir primero" 
+para que aparezca cuando el usuario ejecuta btnExchange
+sin antes haber puesto un valor en los inputs
+
+-Renovar diseño del sitio ENTERO!!!
+
+*/
+
+    
+
